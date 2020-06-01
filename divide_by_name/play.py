35891c8e5449fb3fbfc8fa072c098fe303a367ba
1244 <hi.c@outlook.com>
2020-06-01 18:51:07 +0800

    feat: add column picker

--- divide_by_name/play.py
+++ divide_by_name/play.py
@@ -2,7 +2,11 @@ import os
 from openpyxl import load_workbook, Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 
-filename = "13"
+filename = "08"
+
+az = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+name_pos = az.index("B")
+content_pos = az.index("A")
 
 wb: Workbook = load_workbook(filename=filename + '.xlsx')
 ws: Worksheet = wb.active
@@ -11,14 +15,14 @@ res: dict = {}
 
 for i, cells in enumerate(ws.iter_rows()):
     if i != 0:
-        person_name = cells[12].value
+        person_name = cells[name_pos].value
         if person_name is not None:
             if person_name not in res:
                 res[person_name] = []
             arr: list = res[person_name]
             arr.append(
                 [
-                    cells[4].value,
+                    cells[content_pos].value,
                 ],
             )
 
@@ -28,10 +32,12 @@ if not os.path.exists(filename):
 for name, contents in res.items():
     wbb: Workbook = Workbook()
     wss: Worksheet = wbb.active
+    count = 0
     for rows in contents:
-        print(rows)
-        wss.append(rows)
-    wbb.save(filename="./{path}/{key}.xlsx".format(path=filename, key=name))
+        if rows[0] is not None:
+            wss.append(rows)
+            count = count + len(rows[0].strip().split())
+    wbb.save(filename="./{path}/{key}_{count}.xlsx".format(path=filename, key=name, count=count))
     wbb.close()
 
 wb.close()
